3fff63874e0236f991af13916bfd45396500dd3a
Mohamed Haddi <m.haddi.95@gmail.com>
2020-06-25 03:23:17 +0100

    change method of letter/word spacing in logo

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,7 +1,23 @@
 let logoElement = document.querySelector("#logo");
-logoElement.addEventListener("click", function (e) {
-    e.preventDefault();
+
+/* delaying the logo's hyperlink target (homepage)
+ * until after hover's transition effect, to fix the
+ * problem of the hover effect taking place until
+ * clicking in mobile devices, which results in the
+ * page loading before the transition is finished
+ */
+logoElement.addEventListener("click", (event) => {
+    event.preventDefault();
     setTimeout(function () {
         window.location.href = ".";
     }, 500);
+});
+
+/* copying the intended text instead of the default
+ * one (which is also haddi.me but since i devided it
+ * into spans only parts of it gets copied)
+ */
+logoElement.addEventListener("copy", (event) => {
+    event.preventDefault();
+    event.clipboardData.setData("text", "haddi.me");
 });
